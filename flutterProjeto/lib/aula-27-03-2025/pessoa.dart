677f2ec82178b76e2d315c8ccfda831eb6bbdcce
Luqui05 <lucashtribeiro@hotmail.com>
2025-03-27 22:44:35 -0300

    fix: só comentei o main no pessoa.dart

--- flutterProjeto/lib/aula-27-03-2025/pessoa.dart
+++ flutterProjeto/lib/aula-27-03-2025/pessoa.dart
@@ -1,18 +1,15 @@
 import 'dart:io';
 
 class Pessoa {
-  // Atributos privados
   String _nome;
   int _idade;
   double _peso;
 
-  // Construtor com validação
   Pessoa({required String nome, required int idade, required double peso})
       : _nome = nome,
         _idade = idade,
         _peso = peso;
 
-  // Getter e Setter para nome com validação
   String get nome => _nome;
   set nome(String novoNome) {
     if (novoNome.isNotEmpty) {
@@ -22,7 +19,6 @@ class Pessoa {
     }
   }
 
-  // Getter e Setter para idade com validação
   int get idade => _idade;
   set idade(int novaIdade) {
     if (novaIdade > 0) {
@@ -32,7 +28,6 @@ class Pessoa {
     }
   }
 
-  // Getter e Setter para peso com validação
   double get peso => _peso;
   set peso(double novoPeso) {
     if (novoPeso > 0) {
@@ -42,7 +37,6 @@ class Pessoa {
     }
   }
 
-  // Método para exibir os dados da pessoa
   void exibirDados() {
     print('--- Dados da Pessoa ---');
     print('Nome: $_nome');
@@ -51,29 +45,24 @@ class Pessoa {
   }
 }
 
-void main() {
-  try {
-    // Solicitando o nome
-    print('Digite o nome:');
-    String nome = stdin.readLineSync() ?? '';
-    if (nome.isEmpty) throw Exception('O nome não pode ser vazio.');
+// void main() {
+//   try {
+//     print('Digite o nome:');
+//     String nome = stdin.readLineSync() ?? '';
+//     if (nome.isEmpty) throw Exception('O nome não pode ser vazio.');
 
-    // Solicitando a idade
-    print('Digite a idade:');
-    int idade = int.tryParse(stdin.readLineSync() ?? '') ?? -1;
-    if (idade <= 0) throw Exception('A idade deve ser maior que 0.');
+//     print('Digite a idade:');
+//     int idade = int.tryParse(stdin.readLineSync() ?? '') ?? -1;
+//     if (idade <= 0) throw Exception('A idade deve ser maior que 0.');
 
-    // Solicitando o peso
-    print('Digite o peso:');
-    double peso = double.tryParse(stdin.readLineSync() ?? '') ?? -1;
-    if (peso <= 0) throw Exception('O peso deve ser maior que 0.');
+//     print('Digite o peso:');
+//     double peso = double.tryParse(stdin.readLineSync() ?? '') ?? -1;
+//     if (peso <= 0) throw Exception('O peso deve ser maior que 0.');
 
-    // Criando a instância da classe Pessoa
-    Pessoa pessoa = Pessoa(nome: nome, idade: idade, peso: peso);
+//     Pessoa pessoa = Pessoa(nome: nome, idade: idade, peso: peso);
 
-    // Exibindo os dados da pessoa
-    pessoa.exibirDados();
-  } catch (e) {
-    print('Erro: ${e.toString()}');
-  }
-}
+//     pessoa.exibirDados();
+//   } catch (e) {
+//     print('Erro: ${e.toString()}');
+//   }
+// }
