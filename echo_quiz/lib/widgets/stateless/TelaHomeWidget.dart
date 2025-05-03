3592231641824d1d2f8066de2063af207ba705aa
Luqui-05 <lucashtribeiro@hotmail.com>
2025-05-03 12:11:05 -0300

    feat: Config inicial do projeto
    - Widget principal App
    - Widget da tela Home
    - Widget da tela do Quiz

---
+++ echo_quiz/lib/widgets/stateless/TelaHomeWidget.dart
@@ -0,0 +1,24 @@
+import 'package:echo_quiz/widgets/stateless/TelaQuizWidget.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('EchoQuiz')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const QuizScreen()),
+            );
+          },
+          child: const Text('Começar'),
+        ),
+      ),
+    );
+  }
+}
