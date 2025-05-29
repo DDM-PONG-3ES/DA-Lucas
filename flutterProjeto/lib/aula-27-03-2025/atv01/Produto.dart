de6dbdf22e4d9e49e68262bd7f378e2fdda99340
Luqui-05 <lucashtribeiro@hotmail.com>
2025-05-29 20:19:50 -0300

    deletei uns ngcs

--- flutterProjeto/lib/aula-27-03-2025/atv01/Produto.dart
+++
@@ -1,9 +0,0 @@
-class Produto {
-  double _preco;
-
-  Produto({required double preco}) : _preco = preco;
-
-  void aplicarDesconto() {
-    _preco = _preco * 0.9;
-  }
-}
--- flutterProjeto/lib/aula-27-03-2025/atv01/estado.dart
+++
@@ -1,6 +0,0 @@
-class Estado {
-  String nome;
-  String sigla;
-
-  Estado({required this.nome, required this.sigla});
-}
